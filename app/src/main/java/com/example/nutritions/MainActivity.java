f480565c819744049c3ddf5849ce748de9f51b1b
Daniel Jensen <dje004@uib.no>
2019-06-30 15:32:45 +0200

    Added login activity

--- app/src/main/java/com/example/nutritions/MainActivity.java
+++ app/src/main/java/com/example/nutritions/MainActivity.java
@@ -51,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
