94104d64069c8209c20194e6d57013d0d49f0a5e
Runar Serigstad <runar.serigstad@zendera.no>
2019-07-06 21:32:46 +0200

    Moved UserSettings.java into settings

--- app/src/main/java/com/example/nutritions/settings/UserSettings.java
+++ app/src/main/java/com/example/nutritions/settings/UserSettings.java
@@ -1,28 +1,28 @@
-package com.example.nutritions;
+package com.example.nutritions.settings;
+
+import static com.example.nutritions.settings.StaticDefaults.DEFAULT_GENDER;
+import static com.example.nutritions.settings.StaticDefaults.DEFAULT_HEIGHT;
+import static com.example.nutritions.settings.StaticDefaults.DEFAULT_WEEKLYEXERCISES;
+import static com.example.nutritions.settings.StaticDefaults.DEFAULT_WEIGHT;
 
 public class UserSettings {
-    private final double DEFAULT_WEIGHT = 80;
-    private final double DEFAULT_HEIGHT = 180;
-    private final int DEFAULT_WEEKLYEXERCISES = 3;
-    private final boolean DEFAULT_GENDER = true;
-
     private double weight;
     private double height;
     private boolean male;
     private int weeklyExercises;
 
-    public UserSettings(){
-        this.weeklyExercises=0;
-        this.weight=0;
-        this.height=0;
-        this.male=true;
+    public UserSettings() {
+        this.weeklyExercises = 0;
+        this.weight = 0;
+        this.height = 0;
+        this.male = true;
     }
 
-    public void setDefault(){
-        this.weight=DEFAULT_WEIGHT;
-        this.height=DEFAULT_HEIGHT;
-        this.weeklyExercises=DEFAULT_WEEKLYEXERCISES;
-        this.male=DEFAULT_GENDER;
+    public void setDefault() {
+        this.weight = DEFAULT_WEIGHT;
+        this.height = DEFAULT_HEIGHT;
+        this.weeklyExercises = DEFAULT_WEEKLYEXERCISES;
+        this.male = DEFAULT_GENDER;
     }
 
     public double getWeight() {
