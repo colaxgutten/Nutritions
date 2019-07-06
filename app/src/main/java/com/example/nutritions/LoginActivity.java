afa44080fb9177c2074823e9141b3f56ad56ae67
Runar Serigstad <runar.serigstad@zendera.no>
2019-07-06 16:48:03 +0200

    Fixed register and improved error handling on login

--- app/src/main/java/com/example/nutritions/LoginActivity.java
+++ app/src/main/java/com/example/nutritions/LoginActivity.java
@@ -1,38 +1,39 @@
 package com.example.nutritions;
 
-import android.app.Activity;
-
 import androidx.annotation.NonNull;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthEmailException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseAuthWebException;
 
 public class LoginActivity extends AppCompatActivity {
-
-    private LoginViewModel loginViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        if (mAuth.getCurrentUser()!=null){
+        if (mAuth.getCurrentUser() != null) {
             finishActivity(1);
         }
         final EditText usernameEditText = findViewById(R.id.username);
@@ -44,30 +45,6 @@ public class LoginActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Register button clicked!");
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                mAuth.signInWithEmailAndPassword(usernameEditText.getText().toString(),passwordEditText.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            finish();
-                        } else {showLoginFailed("Failed to log in");
-                        loadingProgressBar.setVisibility(View.INVISIBLE);
-                            System.out.println("Username: "+usernameEditText.getText().toString());
-                            System.out.println("Password: "+passwordEditText.getText().toString());
-                        }
-                    }
-                });
-            }
-        });
-
-
-
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("Login button clicked!");
                 loadingProgressBar.setVisibility(View.VISIBLE);
                 String usernameString = usernameEditText.getText().toString();
                 String passwordString = passwordEditText.getText().toString();
@@ -83,23 +60,56 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
 
-                Task<AuthResult> signIn = mAuth.signInWithEmailAndPassword(usernameString, passwordString);
-                signIn.addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                Task<AuthResult> registerTask = mAuth.createUserWithEmailAndPassword(usernameString, passwordString);
+                registerTask.addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            System.out.println("Success login");
-                            finish();
-                        } else {
-                            System.out.println("Username: "+usernameEditText.getText().toString());
-                            System.out.println("Password: "+passwordEditText.getText().toString());
-                            showLoginFailed("Login failed");
-                            System.out.println(task.getException());
-                            loadingProgressBar.setVisibility(View.INVISIBLE);
-                        }
+                    public void onSuccess(AuthResult authResult) {
+                        finish();
                     }
                 });
+                registerTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        showLoginFailed(e.getLocalizedMessage());
+                        loadingProgressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        });
 
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingProgressBar.setVisibility(View.VISIBLE);
+                String usernameString = usernameEditText.getText().toString();
+                String passwordString = passwordEditText.getText().toString();
+                if (usernameString.equals("")) {
+                    showLoginFailed("Username can't be empty");
+                    loadingProgressBar.setVisibility(View.INVISIBLE);
+                    return;
+                }
+
+                if (passwordString.equals("")) {
+                    showLoginFailed("Password can't be empty");
+                    loadingProgressBar.setVisibility(View.INVISIBLE);
+                    return;
+                }
+
+                Task<AuthResult> signInTask = mAuth.signInWithEmailAndPassword(usernameString, passwordString);
+                signInTask.addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        finish();
+                    }
+                });
+                signInTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        showLoginFailed(e.getLocalizedMessage());
+                        loadingProgressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
             }
         });
     }
