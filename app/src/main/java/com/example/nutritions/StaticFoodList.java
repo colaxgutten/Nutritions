634aee0096ea84597590b3c62e1b9c8d1eb081bf
Runar Serigstad <runar.serigstad@zendera.no>
2019-07-06 21:02:31 +0200

    Removed StaticFoodList.java

--- app/src/main/java/com/example/nutritions/StaticFoodList.java
+++
@@ -1,5 +0,0 @@
-package com.example.nutritions;
-
-public class StaticFoodList {
-    static String[] food = {"brød + smør + leverpostei","brød + leverpostei", "styrk kakao", "lettmelk","melk ekstra lett","skummet melk", "spaghetti capri","grandiosa pepperoni"};
-}
