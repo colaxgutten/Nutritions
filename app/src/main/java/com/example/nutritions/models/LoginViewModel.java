ad04b18e34188710d5eaa6ce077868f2e767b77c
Runar Serigstad <runar.serigstad@zendera.no>
2019-07-06 21:36:53 +0200

    Cleaned up LoginViewModel.java

--- app/src/main/java/com/example/nutritions/models/LoginViewModel.java
+++ app/src/main/java/com/example/nutritions/models/LoginViewModel.java
@@ -1,18 +1,12 @@
 package com.example.nutritions.models;
 
 public class LoginViewModel {
-    private String userName;
-    private String password;
-
-    public LoginViewModel(){
-        userName = "";
-        password = "";
-    }
+    private String userName = "";
+    private String password = "";
 
     public String getUserName() {
         return userName;
     }
-
     public void setUserName(String userName) {
         this.userName = userName;
     }
@@ -20,7 +14,6 @@ public class LoginViewModel {
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
